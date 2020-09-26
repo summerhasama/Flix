ccc5cc924202f2c2515767a749a331b0332c281b
Summer Hasama <summerhasama@gmail.com>
2020-09-25 23:07:23 -0700

    commit

---
+++ Flix/MoviesCell.swift
@@ -0,0 +1,29 @@
+//
+//  MoviesCell.swift
+//  Flix
+//
+//  Created by Summer Hasama on 9/25/20.
+//
+
+import UIKit
+
+class MoviesCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var synopsisLabel: UILabel!
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
